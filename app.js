f0780b97e15432054098a7972c1bead3aaba6e88
Erik Hernandez <erikestuardo_161@hotmail.com>
2020-09-29 21:43:26 -0600

    add function SaveTask

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('formTask').addEventListener('submit',saveTask);
+
+function saveTask(e) {
+
+  let title = document.getElementById('title').value;
+
+    e.preventDefault();
+}
